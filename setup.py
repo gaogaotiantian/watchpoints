7b00d26c70458441a2d9e8009362e8dd3102f951
gaogaotiantian <gaogaotiantian@hotmail.com>
2020-12-07 23:30:09 -0800

    set classifiers

--- setup.py
+++ setup.py
@@ -27,8 +27,13 @@ setuptools.setup(
     package_dir={"":"src"},
     classifiers=[
         "Development Status :: 3 - Alpha",
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent"
     ],
+    python_requires=">=3.6",
 )
